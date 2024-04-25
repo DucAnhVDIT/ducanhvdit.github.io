baf0900bff10ef9f92a48df81ce3dc1e18f9ac5e
Duc Anh VDIT <ducanh.lo@vdit.co.uk>
2024-04-25 17:53:32 +0100

    render from json file for all the cards

--- src/pages/Manager/setting/index.tsx
+++ src/pages/Manager/setting/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function SettingsPage() {
+  useEffect(() => {
+    document.body.style.backgroundColor = 'white';
+    return () => {
+      document.body.style.backgroundColor = ''; // Reset background color when component unmounts
+    };
+  }, []);
   return (
-    <div>
+    <div className='h-full'>
         Setings
     </div>
   )
--- src/pages/Marketing/mainpage.tsx
+++ src/pages/Marketing/mainpage.tsx
@@ -1,29 +1,60 @@
-import Card from '@mui/material/Card/Card'
-import CardActions from '@mui/material/CardActions/CardActions'
-import CardContent from '@mui/material/CardContent/CardContent'
-import React from 'react'
-import Button from '../../base-components/Button'
-import BirthdayReminder from './birthdayReminder'
-import ReturnCustomer from './returnCustomer'
-import ReviewSettings from './reviewSettings'
-import Voucher from './voucher'
-import Campaigns from './campaigns'
-import Templates from './templates'
+import Card from "@mui/material/Card/Card";
+import CardActions from "@mui/material/CardActions/CardActions";
+import CardContent from "@mui/material/CardContent/CardContent";
+import React from "react";
+import Button from "../../base-components/Button";
+import { Grid } from "@mui/material";
+import Data from './marketingData.json'
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import { Typography } from "@mui/material";
+import Lucide from "../../base-components/Lucide";
+
 
 function MainPage() {
   return (
-    <div className='p-7 flex flex-col opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards animate-delay'>
-        <div className='flex justify-between'>
-            <BirthdayReminder />
-            <Voucher />
-            <ReviewSettings />
-        </div>
+    // <div className='p-7 flex flex-col opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards animate-delay'>
+    //     <div className='flex justify-between'>
+    //         <BirthdayReminder />
+    //         <Voucher />
+    //         <ReviewSettings />
+    //     </div>
         
-        <div className='flex justify-between mt-10'>
-            <ReturnCustomer />
-            <Campaigns />
-            <Templates />
-        </div>
+    //     <div className='flex justify-between mt-10'>
+    //         <ReturnCustomer />
+    //         <Campaigns />
+    //         <Templates />
+    //     </div>
+    // </div>
+    <div className="opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards animate-delay">
+    <Grid container spacing={3} style={{margin:"20px"}}>
+      {Data.map((res : any, index:any) => (
+        <Grid item xs={12} sm={4} key={index}>
+          <Card className="w-[80%] mt-3">
+            <CardHeader
+              sx={{ display: "flex", alignItems: "center" }}
+              title={<Typography variant="h6">{res.title}</Typography>}
+              // avatar={
+              //   <Avatar sx={{ backgroundColor: "#1E40AF" }}>
+              //     <Lucide icon={res.icon} />
+              //   </Avatar>
+              // }
+            />
+            <CardContent sx={{ flex: "1 0 auto", minHeight: "100px" }}>
+              <Typography variant="body1" className="text-gray-500">
+                {res.description}
+              </Typography>
+            </CardContent>
+
+            <CardActions sx={{ justifyContent: "flex-end", flex: "0 0 auto" }}>
+              <Button variant="primary" className="w-32">
+                Set Up
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
     </div>
   )
 }
