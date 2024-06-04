1dbb45afd5548abd4d0a707504763dc6deadb2f8
Duc Anh VDIT <ducanh.lo@vdit.co.uk>
2024-06-04 15:02:35 +0100

    Reuseable Blank Page Design

--- src/pages/Marketing/Vouchers/index.tsx
+++ src/pages/Marketing/Vouchers/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import VoucherBlank from "../../../components/BlankPage.tsx/VoucherBlank";
+import voucherImage from '../../../assets/images/VoucherImage.png';
 
 function Voucher() {
+  useEffect(() => {
+    document.body.style.padding = "0";
+
+    document.body.style.backgroundColor = "white";
+
+    return () => {
+      document.body.style.padding = "";
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
+  const listItems = [
+    "Offer flexible voucher options for clients",
+    "Easily track and manage voucher usage",
+    "Redeem vouchers both online and in-store",
+  ];
   return (
-    <div>Voucher</div>
-  )
+    <div className="min-h-screen">
+      <VoucherBlank
+        title={"Vouchers"}
+        description={
+          "Seamlessly manage various aspects of your voucher program."
+        }
+        imageSrc={voucherImage}
+        listItems={listItems}
+      />
+    </div>
+  );
 }
 
-export default Voucher
+export default Voucher;
